3659a07d86b57075714bd509413f87e4b3dbf5f1
kext <luffyke@gmail.com>
2015-05-02 22:26:59 +0800

    init commit

--- ChineseZodiac/ViewController.swift
+++ ChineseZodiac/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var yearOfBirth: UITextField!
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    let offset = 4
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        // hide the number pad
+        yearOfBirth.resignFirstResponder()
+    }
 
+    @IBAction func okDidTap(sender: AnyObject) {
+        yearOfBirth.resignFirstResponder()
+        if let year = yearOfBirth.text.toInt() {
+            var imageNumber = (year - offset) % 12
+            image.image = UIImage(named: String (imageNumber))
+        } else {
+            // notify user to input year
+        }
+    }
 
 }
 
