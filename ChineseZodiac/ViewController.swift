f9c0eaec0fb9b9c0a58645add3aaa862fb03668d
kext <luffyke@gmail.com>
2015-05-02 13:01:38 +0800

    Initial Commit

---
+++ ChineseZodiac/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChineseZodiac
+//
+//  Created by kext on 15/5/2.
+//  Copyright (c) 2015年 smartx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
